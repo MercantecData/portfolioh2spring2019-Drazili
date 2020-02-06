a8e0be32cac7df62b05dbd3a857899ef7a24c04a
Sebastian <im.cool@live.dk>
2020-02-06 14:46:13 +0100

    Library to A&I
    Added the library dll to A&I project.

--- AbstractInterfaces/AbstractInterfaces/Program.cs
+++ AbstractInterfaces/AbstractInterfaces/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraTheLibrary;
 
 namespace AbstractInterfaces
 {
@@ -43,7 +44,8 @@ namespace AbstractInterfaces
                 new Personae("Momina", 92, "kvinde")
             };
 
-
+            LibraTheLibrary.Math math = new LibraTheLibrary.Math();
+            Console.WriteLine(math.Multiply("9", "6"));
             Console.ReadKey();
         }
     }
